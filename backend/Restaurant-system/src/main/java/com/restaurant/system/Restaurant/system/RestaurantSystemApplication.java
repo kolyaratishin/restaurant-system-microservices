5594fc21a25258bff76ff3c57310e3665cfd337d
kolyaratishin <kolyaratishin1@gmail.com>
2022-12-06 12:38:15 +0200

    add discovery service

--- backend/Restaurant-system/src/main/java/com/restaurant/system/Restaurant/system/RestaurantSystemApplication.java
+++ backend/Restaurant-system/src/main/java/com/restaurant/system/Restaurant/system/RestaurantSystemApplication.java
@@ -2,7 +2,9 @@ package com.restaurant.system.Restaurant.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class RestaurantSystemApplication {
 
