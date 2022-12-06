736dcacc59e5a8c48220c65685b51c9f6a0e11a5
kolyaratishin <kolyaratishin1@gmail.com>
2022-12-06 12:38:33 +0200

    add skeleton of statistics service

---
+++ backend/restaurant-statistic-service/src/main/java/com/restaurant/statistics/restaurantstatisticservice/RestaurantStatisticServiceApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.statistics.restaurantstatisticservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantStatisticServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantStatisticServiceApplication.class, args);
+	}
+
+}
---
+++ backend/restaurant-statistic-service/src/test/java/com/restaurant/statistics/restaurantstatisticservice/RestaurantStatisticServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.statistics.restaurantstatisticservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantStatisticServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
